bd778748a476473074f90f7df54b1732399484fc
tiagoCuervo <dscuervog@gmail.com>
2021-05-26 21:16:45 +0200

    Fixed per epoch logging

--- trainer.py
+++ trainer.py
@@ -216,14 +216,12 @@ def trainingLoop(trainDataset,
 
             if log2Board:
                 for t in range(len(locLogsVal["locLoss_val"])):
-                    experiment.log_metric(f"Losses/epoch/locLoss_train_{t}",
-                                          locLogsTrain["locLoss_train"][t] / locLogsTrain['iter'], step=epoch)
-                    experiment.log_metric(f"Accuracy/epoch/locAcc_train_{t}",
-                                          locLogsTrain["locAcc_train"][t] / locLogsTrain['iter'], step=epoch)
-                    experiment.log_metric(f"Losses/epoch/locLoss_val_{t}", locLogsVal["locLoss_val"][t]
-                                          / locLogsVal['iter'], step=epoch)
-                    experiment.log_metric(f"Accuracy/epoch/locAcc_val_{t}", locLogsVal["locAcc_val"][t]
-                                          / locLogsVal['iter'], step=epoch)
+                    experiment.log_metric(f"Losses/epoch/locLoss_train_{t}", locLogsTrain["locLoss_train"][t],
+                                          step=epoch)
+                    experiment.log_metric(f"Accuracy/epoch/locAcc_train_{t}", locLogsTrain["locAcc_train"][t],
+                                          step=epoch)
+                    experiment.log_metric(f"Losses/epoch/locLoss_val_{t}", locLogsVal["locLoss_val"][t], step=epoch)
+                    experiment.log_metric(f"Accuracy/epoch/locAcc_val_{t}", locLogsVal["locAcc_val"][t], step=epoch)
 
             if currentAccuracy > bestAcc:
                 bestStateDict = cpcModel.state_dict()
